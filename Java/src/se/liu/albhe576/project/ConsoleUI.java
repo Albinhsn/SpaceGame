a1949407a1a4e94ad990bc56d232bb7157d69bc0
albinhsn <albin.henningsson1@gmail.com>
2024-03-07 01:19:59 +0100

    some console cleanup

--- Java/src/se/liu/albhe576/project/ConsoleUI.java
+++ Java/src/se/liu/albhe576/project/ConsoleUI.java
@@ -13,61 +13,74 @@ public class ConsoleUI extends UI{
 
     private String input;
     private UIState parent;
-    private final String[] output = new String[7];
+    private final String[] sentCommands = new String[7];
     private final UIComponent background;
     private final UIComponent consoleInput;
 
     public void setParentState(UIState parent){
         this.parent = parent;
     }
-    @Override
-    UIState render(InputState inputState, Renderer renderer, long window, int score, int hp) {
-        // Render like some sort of quad background of grey thingy
-        // Figure out how to render a input field at the bottom
-        // figure out how to store input and render input
-        // figure out how to execute commands :)
 
-        renderer.renderUIComponent(background.textureId, background.x, background.y, background.width, background.height);
-        renderer.renderUIComponent(consoleInput.textureId, consoleInput.x,consoleInput.y, consoleInput.width,consoleInput.height);
-        boolean []releasedState = inputState.getKeyboardStateReleased();
+    private void writeCommand(){
+        for(int i = this.sentCommands.length - 2; i >= 0; i--){
+            this.sentCommands[i + 1] = this.sentCommands[i];
+        }
+        this.sentCommands[0] = this.input;
+        this.input = "";
+    }
+
+    private void addInput(boolean [] releasedState){
         for(int i = 0; i < releasedState.length; i++){
             if(releasedState[i]){
-                System.out.printf("Found '%c' %d\n", (char)i, i);
                 this.input = this.input.concat(String.valueOf((char)i));
             }
         }
-        if(inputState.isBackspaceReleased() && !this.input.isEmpty()){
-            this.input = this.input.substring(0, this.input.length() - 1);
-        }
-        if(inputState.isEnterReleased()){
-            if(this.input.equals("QUIT")){
-                this.input = "";
-                Arrays.fill(this.output, "");
-                return this.parent;
-            }
-            for(int i = this.output.length - 2; i >= 0; i--){
-                this.output[i + 1] = this.output[i];
-            }
-            this.output[0] = this.input;
+    }
+    private UIState executeCommands(){
+        if(this.input.equals("QUIT")){
             this.input = "";
+            Arrays.fill(this.sentCommands, "");
+            return this.parent;
+        }
+        return null;
+    }
+    @Override
+    UIState render(InputState inputState, Renderer renderer, long window, int score, int hp) {
+        renderer.renderUIComponent(background.textureId, background.x, background.y, background.width, background.height);
+        renderer.renderUIComponent(consoleInput.textureId, consoleInput.x,consoleInput.y, consoleInput.width,consoleInput.height);
+
+        float spaceSize = ResourceManager.STATE_VARIABLES.get("fontSpaceSizeSmall");
+        float fontSize  = ResourceManager.STATE_VARIABLES.get("fontFontSizeMedium");
+        final float x   = this.consoleInput.x - this.consoleInput.width;
+        float y         = this.consoleInput.y;
+
+        this.addInput(inputState.getKeyboardStateReleased());
+        if(inputState.isEnterReleased()){
+            UIState out = this.executeCommands();
+            if(out != null){
+                return out;
+            }
+            this.writeCommand();
         }
 
 
         if(!this.input.isEmpty()){
-            renderer.renderText(this.input, -50.0f, -40.0f, ResourceManager.STATE_VARIABLES.get("fontSpaceSizeSmall"), ResourceManager.STATE_VARIABLES.get("fontFontSizeMedium"), Color.BLACK, false);
+            renderer.renderText(this.input, x, y, spaceSize, fontSize, Color.BLACK, false);
         }
 
-        for(int i = 0, y = -28; i < this.output.length; i++, y += 12){
-            renderer.renderText(this.output[i], -50.0f, (float)y, ResourceManager.STATE_VARIABLES.get("fontSpaceSizeSmall"), ResourceManager.STATE_VARIABLES.get("fontFontSizeMedium"), Color.BLACK, false);
+        for(String command : this.sentCommands){
+            y += fontSize * 2;
+            renderer.renderText(command, x, y, spaceSize, fontSize, Color.BLACK, false);
         }
 
         return UIState.CONSOLE;
     }
 
     public ConsoleUI(ResourceManager resourceManager){
-        this.background = new UIComponent(0, 0, 50.0f, 50.0f, resourceManager.textureIdMap.get(Texture.GREY_BUTTON_05).textureId);
-        this.consoleInput = new UIComponent(0, -40.0f, 50.0f, 10.0f, resourceManager.textureIdMap.get(Texture.GREY_BUTTON_14).textureId);
-        this.input = "";
-        Arrays.fill(this.output, "");
+        this.background     = new UIComponent(0, 0, 50.0f, 50.0f, resourceManager.textureIdMap.get(Texture.GREY_BUTTON_05).textureId);
+        float fontSize  = ResourceManager.STATE_VARIABLES.get("fontFontSizeMedium");
+        this.consoleInput   = new UIComponent(0, -40.0f, 50.0f, fontSize * 1.5f, resourceManager.textureIdMap.get(Texture.GREY_BUTTON_14).textureId);
+        this.input          = "";
+        Arrays.fill(this.sentCommands, "");
     }
 }
